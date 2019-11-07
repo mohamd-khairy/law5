edd70d1abc103ac7d0d79d46160b49bcd37fef51
mohamd-khairy <red.devile2011@gmail.com>
2019-11-07 08:29:13 +0200

    change db

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE', 'law5'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '2iA1-##0%;76HQd'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
